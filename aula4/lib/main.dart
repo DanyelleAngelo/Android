6d721500631de9f77dbeaaee4a9bf8c41fe64814
DanyelleAngelo <danyangelofsa@gmail.com>
2020-09-04 13:07:19 -0300

    Aula4

---
+++ aula4/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:aula4/view/lista_curso.dart';
+
+void main() {
+  runApp((MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.green[900],
+      accentColor: Colors.blueAccent[700],
+      buttonTheme: ButtonThemeData(
+        buttonColor: Colors.blueAccent[700],
+        textTheme: ButtonTextTheme.primary,
+      )
+    ) ,
+    home: Home(),
+  )));
+}
+
