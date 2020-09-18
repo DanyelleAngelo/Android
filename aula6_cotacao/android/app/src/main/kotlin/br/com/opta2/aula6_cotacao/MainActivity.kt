8bed632f743bb68ae1eaf58dd3adeb4af71b999d
DanyelleAngelo <danyangelofsa@gmail.com>
2020-09-18 08:45:41 -0300

    Aula 6 e aula 7

---
+++ aula6_cotacao/android/app/src/main/kotlin/br/com/opta2/aula6_cotacao/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.opta2.aula6_cotacao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aula7_gastMensal/android/app/src/main/kotlin/br/com/opta2/aula7_gastMensal/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.opta2.aula7_gastMensal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
